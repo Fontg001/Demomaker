cdbf02263c1ced90e150848cd87237780c96d512
Ignacio <juanignaciofont713@gmail.com>
2024-10-21 01:44:21 -0300

    First commit

---
+++ src/main/java/com/demo/Demomaker/DemomakerApplication.java
@@ -0,0 +1,13 @@
+package com.demo.Demomaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemomakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemomakerApplication.class, args);
+	}
+
+}
